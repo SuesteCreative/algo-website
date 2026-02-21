a1760bdf4808d78a1f825e976778270fbd6110fb
Pedro Porto <pedrotovarporto@gmail.com>
2026-02-21 18:45:19 +0000

    feat: improve Decap CMS back-office branding and expand editable regular content

--- _data/site.js
+++ _data/site.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 module.exports = function () {
     let settings = {};
+    let about = {};
+
     try {
         const settingsPath = path.join(__dirname, "../content/settings/general.yml");
         const fileContents = fs.readFileSync(settingsPath, "utf8");
@@ -12,8 +14,17 @@ module.exports = function () {
         console.error("Error loading settings from YAML:", e);
     }
 
+    try {
+        const aboutPath = path.join(__dirname, "../content/settings/about.yml");
+        const aboutContents = fs.readFileSync(aboutPath, "utf8");
+        about = yaml.load(aboutContents);
+    } catch (e) {
+        console.error("Error loading about from YAML:", e);
+    }
+
     return {
         ...settings,
+        about: about,
         currentYear: new Date().getFullYear(),
         siteUrl: "https://algoatelier.pt",
         siteName: "Algo Atelier"
